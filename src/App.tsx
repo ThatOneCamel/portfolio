e8d355ea21f777e60ff08dfff1dda6bc1531b0d0
ThatOneCamel <thomascameront@gmail.com>
2023-09-23 01:37:26 -0500

    Add various icons, HoverIcon, and theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,118 @@
-import { MantineProvider, createStyles, Tabs, Flex, Center, Container } from '@mantine/core';
+import { MantineProvider, ColorSchemeProvider, ColorScheme, createStyles, Tabs, Flex, Center, Container } from '@mantine/core';
+import { useColorScheme } from '@mantine/hooks';
+import { useState } from 'react';
+
 import ProjectDisplay from "./components/ProjectDisplay";
 import jsonData from "./dat.json";
 import jsonGuideData from "./guides_dat.json";
 import BioSection from './components/Bio';
 import GuideDisplay from './components/GuideDisplay';
 
+import { ReactComponent as Dalamud } from './components/icons/dalamud.svg';
+import { ReactComponent as Plumbob } from './components/icons/plumbob.svg';
+
+import {
+  DiAndroid,
+  DiApple,
+  DiCss3,
+  DiGit,
+  DiHtml5,
+  DiNodejs,
+  DiRust,
+  DiSwift,
+  DiVisualstudio
+  } from "react-icons/di";
+
+import {
+  FaBuildingColumns,
+  FaJava,
+  FaRegBuilding
+  } from "react-icons/fa6";
+
+import { BiLogoGoLang, BiLogoUnity } from "react-icons/bi";
+
+import {
+  IconApi,
+  IconBrandCSharp,
+  IconBrandCpp,
+  IconLetterC
+  } from '@tabler/icons-react';
+
+import {
+  SiAngular,
+  SiAdobephotoshop,
+  SiDjango,
+  SiDocker,
+  SiDotnet,
+  SiFirebase,
+  SiGrafana,
+  SiGodotengine,
+  SiJavascript,
+  SiKubernetes,
+  SiLua,
+  SiMicrosoftazure,
+  SiPowershell,
+  SiPrometheus,
+  SiPython,
+  SiReact,
+  SiSketch,
+  SiTypescript,
+  SiXcode,
+  SiYubico
+  } from "react-icons/si";
+
+//let iconMap = new Map<string, IconData>();
+//iconMap.set("Angular", {name: "Angular", icon: <HoverIcon name='Angular' icon={<SiAngular/>}/>})
+
+export const iconMap = new Map<string, JSX.Element>([
+  ["Final Fantasy XIV", <Dalamud fill="white" width="1.5em" height="1.5em"/>],
+  ["The Sims 2", <Plumbob strokeWidth={7} width="1.5em" height="1.5em"/>],
+
+  ["Android",      <DiAndroid/>],
+  ["CSS",          <DiCss3/>],
+  ["Git",          <DiGit/>],
+  ["HTML",         <DiHtml5/>],
+  ["iOS",          <DiApple/>],
+  ["MacOS",        <DiApple/>],
+  ["NodeJS",       <DiNodejs/>],
+  ["Rust",         <DiRust/>],
+  ["Swift",        <DiSwift/>],
+  ["VisualStudio", <DiVisualstudio/>],
+
+  ["Java",         <FaJava/>],
+  ["University",   <FaBuildingColumns/>],
+  ["Office",       <FaRegBuilding/>],
+
+  ["Go",           <BiLogoGoLang/>],
+  ["Unity",        <BiLogoUnity/>],
+
+  ["REST",         <IconApi/>],
+  ["Cpp",          <IconBrandCpp/>],
+  ["CSharp",       <IconBrandCSharp/>],
+  ["C",            <IconLetterC/>],
+
+  ["Angular",      <SiAngular/>],
+  ["Photoshop",    <SiAdobephotoshop/>],
+  ["Django",       <SiDjango/>],
+  ["Docker",       <SiDocker/>],
+  ["DotNet",       <SiDotnet size="24"/>],
+  ["Firebase",     <SiFirebase/>],
+  ["Grafana",      <SiGrafana/>],
+  ["Godot",        <SiGodotengine/>],
+  ["JavaScript",   <SiJavascript/>],
+  ["Kubernetes",   <SiKubernetes/>],
+  ["Lua",          <SiLua/>],
+  ["Azure",        <SiMicrosoftazure/>],
+  ["Powershell",   <SiPowershell/>],
+  ["Prometheus",   <SiPrometheus/>],
+  ["Python",       <SiPython/>],
+  ["React",        <SiReact/>],
+  ["React-Native", <SiReact/>],
+  ["Sketch",       <SiSketch/>],
+  ["TypeScript",   <SiTypescript/>],
+  ["Xcode",        <SiXcode/>],
+  ["YubiKey",      <SiYubico/>],
+]);
 const useStyles = createStyles((theme) => ({
   container: {
     height: "auto",
@@ -37,8 +145,6 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-
-
 export default function App() {
   const { classes } = useStyles()
 
@@ -52,37 +158,26 @@ export default function App() {
   for (const [, obj] of Object.entries(jsonGuideData)) {
     guideArr.push(<GuideDisplay project={obj}/>)
   }
+
+  const preferredColorScheme = useColorScheme();
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS theme={{colorScheme: 'dark'}}>
-      <BioSection/>
-      <Tabs defaultValue="projects" color="green" mt="xs">
-        <Center>
-        <Tabs.List grow position="center" w={{sm: "50%", base: "90%"}} mb="sm">
-          <Tabs.Tab value='projects'>Projects</Tabs.Tab>
-          <Tabs.Tab value='guides'>Guides</Tabs.Tab>
-        </Tabs.List>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider withGlobalStyles withNormalizeCSS theme={{colorScheme}}>
+        <BioSection/>
+        <Tabs defaultValue="projects" color="green" mt="xs">
+          <Center>
+          <Tabs.List grow position="center" w={{sm: "50%", base: "90%"}} mb="sm">
+            <Tabs.Tab value='projects'>Projects</Tabs.Tab>
+            <Tabs.Tab value='guides'>Guides</Tabs.Tab>
+          </Tabs.List>
 
-        </Center>
-
-        <Tabs.Panel value="projects">
-          <Flex
-            mih={50}
-            gap="md"
-            justify="center"
-            align="flex-start"
-            direction="row"
-            wrap="wrap"
-          >
-            {arr}
-          </Flex>
-        </Tabs.Panel>
-
-        <Tabs.Panel value="guides">
-          <Center mb="sm">
-            This is just as much for myself as it is for anyone else :D
           </Center>
-          <Container pb="lg" w="100%">
+
+          <Tabs.Panel value="projects">
             <Flex
               mih={50}
               gap="md"
@@ -91,11 +186,29 @@ export default function App() {
               direction="row"
               wrap="wrap"
             >
-              {guideArr}
+              {arr}
             </Flex>
-          </Container>
-        </Tabs.Panel>
-      </Tabs>
-    </MantineProvider>
+          </Tabs.Panel>
+
+          <Tabs.Panel value="guides">
+            <Center m="sm">
+              This is just as much for myself as it is for anyone else :D
+            </Center>
+            <Container pb="lg" w="100%">
+              <Flex
+                mih={50}
+                gap="md"
+                justify="center"
+                align="flex-start"
+                direction="row"
+                wrap="wrap"
+              >
+                {guideArr}
+              </Flex>
+            </Container>
+          </Tabs.Panel>
+        </Tabs>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,46 +1,120 @@
-import { UnstyledButton, Image, Flex, Box, Title, Text, Divider, Center } from '@mantine/core';
+import {
+  createStyles,
+  ActionIcon,
+  useMantineColorScheme,
+  UnstyledButton,
+  Image,
+  Flex,
+  Box,
+  Title,
+  Text,
+  Divider,
+  Group,
+  Stack,
+} from "@mantine/core";
+
+import { IconSun, IconMoonStars } from "@tabler/icons-react";
+
+const useStyles = createStyles((theme, _params, getRef) => ({
+  wrapper: {
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark : theme.colors.gray[1],
+  },
+}));
 
 export default function BioSection() {
-    return (
-        <Center bg="dark">
-            <Flex
-                mih={50}
-                gap={{ base: 0, sm: "md" }}
-                justify="center"
-                align="center"
-                direction={{ base: 'column', sm: 'row' }}
-                wrap="nowrap"
-                mt="lg"
-                mb={5}
-                mx={20}
-            >
-                <Box sx={{maxHeight: 400, maxWidth: 400, paddingBottom: 10}} px={0}>
-                    <UnstyledButton component='a' href="https://github.com/ThatOneCamel" target="_blank" rel="noopener noreferrer">
-                        <Image mt="xs" style={{minWidth:200, maxWidth: 300, borderColor: "rgba(255, 255, 255, .75)", borderWidth: 3, borderStyle: "solid", borderRadius: "50%"}} src="/assets/avatar.png" radius={300} fit="cover"/>
-                    </UnstyledButton>
-                </Box>
-                <Divider size="xs" orientation="vertical" />
-                <Flex
-                        mih={50}
-                        gap="xs"
-                        justify={{ base: 'center', sm: 'flex-start' }}
-                        direction="column"
-                        wrap="nowrap"
-                        mb={30}
-                    >
-                        <Title /*color="#F2E2D2"*/ mb={0} order={1} size={45} weight={700} ta={{ base: "center", sm: "left"}}>Camel</Title>
-                        <Text mt={0}>I tell computers to do stuff and break things along the way</Text>
-                        <Text mt={0}>I like to think make things that make my life easier and/or more entertaining </Text>
-                        <Text mt={0}>
-                            I'm a Computer Science graduate that's been in the workforce since the pandemic hit.
-                            I'm a backend engineer with a current emphasis in containerized technology.
-                            (Kubernetes, Docker, OpenShift, etc.)
-                        </Text>
-                        <Text mt={0}>For better or worse, I'm here and thanks for visiting my page. I hope it proves informative, if not useful</Text>
-                </Flex>
-            </Flex>
-        </Center>
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
 
-    );
+  const { classes } = useStyles();
+
+  return (
+    <Stack className={classes.wrapper} /*bg="dark"*/>
+      <Flex
+        mih={50}
+        gap={{ base: 0, sm: "md" }}
+        justify="center"
+        align="center"
+        direction={{ base: "column", sm: "row" }}
+        wrap="nowrap"
+        mt="lg"
+        mb={5}
+        mx={20}>
+        <Box sx={{ maxHeight: 400, maxWidth: 400, paddingBottom: 10 }} px={0}>
+          <UnstyledButton
+            component="a"
+            href="https://github.com/ThatOneCamel"
+            target="_blank"
+            rel="noopener noreferrer">
+            <Image
+              mt="xs"
+              style={{
+                minWidth: 200,
+                maxWidth: 300,
+                borderColor: "rgba(255, 255, 255, .75)",
+                borderWidth: 3,
+                borderStyle: "solid",
+                borderRadius: "50%",
+              }}
+              src="/assets/avatar.png"
+              radius={300}
+              fit="cover"
+            />
+          </UnstyledButton>
+        </Box>
+
+        <Divider size="xs" orientation="vertical" />
+
+        <Flex
+          mih={50}
+          gap="xs"
+          justify={{ base: "center", sm: "flex-start" }}
+          direction="column"
+          wrap="nowrap"
+          mb={30}>
+          <Group position="apart">
+            <Title
+              /*color="#F2E2D2"*/
+              mb={0}
+              order={1}
+              size={45}
+              weight={700}
+              ta={{ base: "center", sm: "left" }}>
+              Camel
+            </Title>
+
+            <ActionIcon
+              variant="outline"
+              color={dark ? "yellow" : "blue"}
+              onClick={() => toggleColorScheme()}
+              title="Toggle color scheme">
+              {dark ? <IconSun /> : <IconMoonStars />}
+            </ActionIcon>
+          </Group>
+
+          <Text mt={0}>
+            I tell computers to do stuff and break things along the way
+          </Text>
+
+          <Title order={2}>About Me</Title>
+
+          <Text mt={0}>
+            I like to think make things that make my life easier and/or more
+            entertaining{" "}
+          </Text>
+
+          <Text mt={0}>
+            I'm a Computer Science graduate that's been in the workforce since
+            the pandemic hit. I'm a backend engineer with a current emphasis in
+            containerized technology. (Kubernetes, Docker, OpenShift, etc.)
+          </Text>
+          
+          <Text mt={0}>
+            For better or worse, I'm here and thanks for visiting my page. I
+            hope it proves informative, if not useful
+          </Text>
+        </Flex>
+      </Flex>
+    </Stack>
+  );
 }
-
